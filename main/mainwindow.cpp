18529e990c752d028f6618946b2382d96809534f
Sushant Paudel <sushant@yarsa.io>
2018-01-06 18:02:16 +0545

    Layout finished | Adding Image on the view port using Button

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <iostream>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QPixmap>
+using namespace std;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +15,18 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_buttonAdd_clicked()
+{
+    try{
+        QPixmap pix("C:\\Users\\susha\\Qt Projects\\major-project\\database\\test-data\\upload.png");
+        QGraphicsScene *scene = new QGraphicsScene(this);
+        ui->imageViewSignature->setScene(scene);
+        scene->addPixmap(pix);
+        ui->imageViewSignature->fitInView(scene->sceneRect(),Qt::KeepAspectRatio);
+        qInfo("Image shown in the view port!");
+    }catch(...){
+        qInfo("ERROR Detected");
+    }
 }
